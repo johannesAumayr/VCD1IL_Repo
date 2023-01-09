0355fb1936e470a6054aaae4f54c772a96217bca
johannesAumayr <116298624+johannesAumayr@users.noreply.github.com>
2023-01-09 10:40:11 +0100

    Update BrakeDistanceCalculator.py

--- BrakeDistanceCalculator.py
+++ BrakeDistanceCalculator.py
@@ -1,3 +1,12 @@
+'''
+Coding assignement - Brake distance simulation
+
+Johannes Aumayr
+s2210710043
+
+NOTE: There might be similarities in the code and the sources with Mr. Hoffmann's coding submission due to discussion and alignments. Anyhow, coding was done independently.
+'''
+
 # libaries
 import numpy as np
 import math
